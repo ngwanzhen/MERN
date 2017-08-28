19b51757cd439723e22c722864d9b8a95fda6014
Wanzhen Ng <ngwanzhen@gmail.com>
2017-08-28 13:09:24 +0800

    commented mongoose

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var express = require('express'),
   glob = require('glob'),
   mongoose = require('mongoose');
 
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
+// mongoose.connect(config.db);
+// var db = mongoose.connection;
+// db.on('error', function () {
+//   throw new Error('unable to connect to database at ' + config.db);
+// });
 
 var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
@@ -22,4 +22,3 @@ module.exports = require('./config/express')(app, config);
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
-
